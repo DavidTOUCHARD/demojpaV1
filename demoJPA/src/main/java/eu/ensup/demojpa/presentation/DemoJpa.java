c29cf63eb5563dca8a957490aa2422d42935573c
David TOUCHARD <david.touchard@hotmail.fr>
2020-02-19 14:11:49 +0100

    first commit

---
+++ demoJPA/src/main/java/eu/ensup/demojpa/presentation/DemoJpa.java
@@ -0,0 +1,36 @@
+package eu.ensup.demojpa.presentation;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import eu.ensup.demojpa.domaine.Formation;
+
+public class DemoJpa {
+
+	public static void main(String[] args) {
+
+		// 1 : Ouverture unit� de travail JPA
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("demojpa-pu");
+		EntityManager em = emf.createEntityManager();
+
+		// 2 : Ouverture transaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// 3 : Instanciation Objet m�tier
+		Formation formation = new Formation("JPA");
+
+		// 4 : Persistance Objet/Relationnel : cr�ation d'un enregistrement en
+		// base
+		em.persist(formation);
+
+		// 5 : Fermeture transaction
+		tx.commit();
+
+		// 6 : Fermeture unit� de travail JPA
+		em.close();
+		emf.close();
+	}
+}
